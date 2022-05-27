dbac4341c7c22e36b3e8c9152c337d31cccae733
jahmagnus <jahmagnus@gmail.com>
2022-05-28 00:33:56 +1000

    mapped project array of JSON objects to component props

--- frontend/src/Components/Projects/ProjectContainer.js
+++ frontend/src/Components/Projects/ProjectContainer.js
@@ -1,30 +1,29 @@
 import React from "react";
 import "./ProjectTemplate/ProjectTemplate.css";
 
-const ProjectContainer = ({header, description, imgsrc, technologies}) => {
+const ProjectContainer = ({ header, description, imgsrc, technologies }) => {
   return (
+
+
     <div className="item">
-    <div className="ui small image">
-      <img src="https://www.fillmurray.com/200/300"/>
-    </div>
-    <div className="middle aligned content">
-      <div className="header">
-        Content A
+      <div className="ui small image">
+        <img src={imgsrc} alt={imgsrc} />
       </div>
-      <div className="description">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed ultricies augue. In efficitur, est non pulvinar rutrum, erat leo consequat diam, sed blandit magna tortor vel felis. 
-          Praesent vitae orci felis. className aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-          </p>
-      </div>
-      <div className="extra">
-        <div className="ui center floated primary button">
-        <i className="fa-brands fa-github"></i> Code
+      <div className="middle aligned content">
+        <div className="header" header={header}>{header}</div>
+        <div className="description">
+          <p description={description} technologies={technologies}>{description}</p>
         </div>
-        <div className="ui center floated secondary black button">
-        <i className="fa-solid fa-laptop-code"></i> Live
+        <div className="extra">
+          <div className="ui center floated primary button">
+            <i className="fa-brands fa-github"></i> Code
+          </div>
+          <a href ="http://west-hampstead-departures.herokuapp.com/homepage.html"><div className="ui center floated secondary black button">
+            <i className="fa-solid fa-laptop-code"></i> Live
+          </div>
+          </a>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- frontend/src/Components/Projects/ProjectTemplate/ProjectTemplate.js
+++ frontend/src/Components/Projects/ProjectTemplate/ProjectTemplate.js
@@ -6,14 +6,30 @@ import './ProjectTemplate.css'
 
 const projects = [{
   projectHeader: 'Departures Web App',
-  description: "During my time working on the railway I developed a departure board web application to ensure I had access to information while working in different areas of the stations and the route where departure boards aren't available",
+  description: "During my time working on the railway I developed a departure board web application to ensure I had access to up to date information for my home station while I am concentrating on studying before going to work - I would keep this web app open on a spare monitor to keep track of delays and cancelations",
   tech: 'Javascript / React / HTML / CSS / REST / Expressjs',
-  img: 'https://placekitten.com/640/360'
+  img: "https://www.fillmurray.com/200/300"
+}, 
+
+{
+  projectHeader: 'Coming Soon',
+  description: "New Portfolio project currently under development",
+  tech: 'Javascript / React / HTML / CSS / REST / Expressjs',
+  img: "https://www.fillmurray.com/200/300"
 }
 ]
 
 
+
+
 const ProjectTemplate = () => {
+
+  let renderedList = []
+
+  renderedList = projects.map((project) => {
+    return <ProjectContainer key={project.projectHeader} header={project.projectHeader} description={project.description} imgsrc={project.img}/>
+  })
+
   return (
     <div
       className="ui inverted vertical masthead center aligned segment"
@@ -23,8 +39,10 @@ const ProjectTemplate = () => {
         <div className="column ten wide grid-column">
         <div className="row">
         <div class="ui items">
-          <ProjectContainer/>
-          <ProjectContainer/>
+
+          {renderedList}
+          {/* <ProjectContainer header={projects[0].projectHeader} description={projects[0].description} imgsrc={projects[0].img}/>
+          <ProjectContainer header={projects[1].projectHeader} description={projects[1].description} imgsrc={projects[1].img}/> */}
           </div>
           </div>
     </div>
